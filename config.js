56a6190a8147c67f5a4f35b06828c1b11912cb96
Ihor Riabokon <igor-18883@mail.ru>
2017-04-10 21:01:20 +0300

    commit

---
+++ config.js
@@ -0,0 +1,6 @@
+﻿module.exports = {
+  consumer_key: '1FsELCn6rlBHX4mtJI3th3LjS',  
+  consumer_secret: 'ypwz8diHm1AeTRt2TIm5h50E2MhZyw4y2Ilh3MYH4T6wYoIN1L',
+  access_token: '851444651206873088-S9R1a6VwaLga20MRjmWMw23XSAhxhz4',  
+  access_token_secret: 'UwmtN5UQU9RQeUGbqtG7xDeZ17pccmW0yj1zZHBv7aTbG'
+}
